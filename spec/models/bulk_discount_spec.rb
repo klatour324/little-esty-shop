f124102db6ba3d8758a8c393ed4f0d491296772b
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-03-07 15:56:09 -0600

    Add validations to bulk discount model class, all model tests pass.

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe BulkDiscount, type: :model do
   describe "relationships" do
     it { should belong_to :merchant}
   end
+
+  describe "validations" do
+    it { should validate_presence_of :quantity_treshold }
+    it { should validate_presence_of :percentage_discount}
+  end
 end
