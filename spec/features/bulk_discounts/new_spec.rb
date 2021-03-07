605a9c8a5c6946f5b7cd14f4e0cc2d6829d76f7a
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-03-07 14:32:45 -0600

    Fix test syntax.

--- spec/features/bulk_discounts/new_spec.rb
+++ spec/features/bulk_discounts/new_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Merchant Bulk Discount New Page" do
+  before :each do
+    @merchant1 = create(:merchant)
+  end
   describe "As a merchant when I visit my bulk discounts index" do
     describe "Then I see a link to create a new discount" do
       it "by cliking this link I am taken to a new page where I see a form to add a new bulk discount" do
@@ -8,7 +11,7 @@ RSpec.describe "Merchant Bulk Discount New Page" do
           within "#new-bulk-discount" do
             expect(page).to have_link("Create Bulk Discount")
             click_on("Create Bulk Discount")
-            expect(current_path).to eq("/merchant/#{merchant1.id}/new")
+            expect(current_path).to eq("/merchant/#{@merchant1.id}/bulk_discounts/new")
           end
       end
     end
@@ -16,13 +19,13 @@ RSpec.describe "Merchant Bulk Discount New Page" do
   describe "As a merchant I can create a new record" do
     describe "When I visit a merchant new page for bulk discount" do
       it "there is a form I can fill out with valid data and I can see the record created" do
-        visit "/merchant/#{merchant1.id}/new"
+        visit "/merchant/#{@merchant1.id}/bulk_discounts/new"
           within "#bulk-discount-form" do
             fill_in "quantity_treshold", with: 20
             fill_in "percentage_discount", with: 10
             click_button("Submit")
           end
-          expect(current_path).to eq("/merchant/#{merchant1.id}/bulk_discounts}")
+          expect(current_path).to eq("/merchant/#{@merchant1.id}/bulk_discounts}")
           within "#all-merchant-bulk-discounts" do
             expect(page).to have_content(10)
             expect(page).to have_content(20)
