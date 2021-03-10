db14efda8fb43802db8d0fc631e03b85471f45d8
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-03-09 18:21:47 -0600

    Add relationship to BulkDiscount, all tests pass.

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,5 +1,7 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+  has_many :items, through: :merchant
+  has_many :invoice_items, through: :items
 
   validates :quantity_treshold, :percentage_discount, presence: true
 end
